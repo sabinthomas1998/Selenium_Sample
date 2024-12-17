928e32f5848fa8671e4582282178ac44b0d8010e
sabin <sabin@MSI>
2024-12-17 14:24:24 +0530

    Update

--- Selenium_Sample/src/main/java/sequencepipeline/TestClass1.java
+++ Selenium_Sample/src/main/java/sequencepipeline/TestClass1.java
@@ -1,12 +1,18 @@
 package sequencepipeline;
 
 import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
 
 public class TestClass1 {
-    public static void main(String[] args) {
+	
+	@Test
+    public void test() {
         
         WebDriver driver = new ChromeDriver();
 
